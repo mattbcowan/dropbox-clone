db7344f0352b6f6c2d84bd60a103ad1edbbe9d7d
mattbcowan <matt@mattbcowan.com>
2019-04-02 18:19:08 -0500

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express'),
+      mongoose = require('mongoose'),
       app = express(),
       port = 3000
 
-  app.get('/', (req, res) => res.send('This is a dropbox clone'))
+mongoose.connect('mongodb+srv://mattcowan:Brokenchains1@mydata-xomta.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 
-  app.listen(port, () => console.log(`Dropbox clone is listening on port ${port}`))
+app.get('/', (req, res) => res.send('This is a dropbox clone'))
+
+app.listen(port, () => console.log(`Dropbox clone is listening on port ${port}`))
