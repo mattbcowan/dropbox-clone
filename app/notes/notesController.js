d8fdb8e742745f137dd13ac4e0808627c0b2edf3
mattbcowan <matt@mattbcowan.com>
2019-04-03 06:44:48 -0500

    Basic CRUD Added

--- app/notes/notesController.js
+++ app/notes/notesController.js
@@ -29,20 +29,92 @@ exports.create = (req, res) => {
 
 // Retrieve and return all Notes from database
 exports.findAll = (req, res) => {
-
+  Note.find()
+  .then(notes => {
+    res.send(notes);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message: err.message || "Some error occurred while retrieving the notes."
+    })
+  })
 };
 
 // Find a single Note with a noteId
 exports.findOne = (req, res) => {
-
+  Note.findById(req.params.noteId)
+  .then(note => {
+    if(!note) {
+      return res.status(404).send({
+        message: `Note not found with ID ${req.params.noteId}`
+      })
+    }
+    res.send(note);
+  })
+  .catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: `Note not found with ID ${req.params.noteId}`
+      });
+    }
+    return res.status(505).send({
+      message: `Error retrieving note with ID ${req.params.noteId}`
+    });
+  });
 };
 
 // Update a note identified by a noteId in the request
 exports.update = (req, res) => {
-
+  // Validate request
+  if(!req.body.content) {
+    return res.status(400).send({
+      message: 'Note cannot be empty'
+    });
+  }
+  // Find note and update it with req.body
+  Note.findByIdAndUpdate(req.params.noteId, {
+    title: req.body.title || 'Untitled Note',
+    content: req.body.content
+  }, {new: true})
+  .then(note => {
+    if(!note) {
+      return res.status(404).send({
+        message: `Note not found with ID ${req.params.noteId}`
+      })
+    }
+    res.send(note);
+  })
+  .catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: `Note not found with ID ${req.params.noteId}`
+      })
+    }
+    return res.status(500).send({
+      message: `Error updating note with ID ${req.params.noteId}`
+    });
+  });
 };
 
 // Delete a note with the specified noteId in the request
 exports.delete = (req, res) => {
-
+  Note.findByIdAndRemove(req.params.noteId)
+  .then(note => {
+    if(!note) {
+      return res.status(404).send({
+        message: `Note not found with ID ${req.params.noteId}`
+      });
+    }
+    res.send({ message: `Note deleted Successfully`})
+  })
+  .catch(err => {
+    if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+      return res.status(404).send({
+        message: `Note not found with ID ${req.params.noteId}`
+      });
+    }
+    return res.status(500).send({
+      message: `Could not delete note with ID ${req.params.noteId}`
+    });
+  });
 };
