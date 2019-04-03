353e498e8f65e0d00bf9f159778e481320e0daf3
mattbcowan <matt@mattbcowan.com>
2019-04-02 20:16:57 -0500

    Add Create and Save note

--- app/notes/notesController.js
+++ app/notes/notesController.js
@@ -3,6 +3,28 @@ const Note = require('./notesModel.js');
 // Create and Save a new Note
 exports.create = (req, res) => {
 
+  // Validate a request
+  if(!req.body.content) {
+    return res.status(400).send({
+      message: "Note content cannot be empty."
+    })
+  }
+
+  // Create a new note
+  const note = new Note({
+    title: req.body.title || "Untitled Note",
+    content: req.body.content
+  });
+
+  // Save Note in the database
+  note.save()
+  .then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || "Some error occurred while creating the Note"
+    });
+  });
 };
 
 // Retrieve and return all Notes from database
