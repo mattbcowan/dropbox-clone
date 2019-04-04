d6b262889bbc49e7676bb380f27b4e93e819aea6
mattbcowan <matt@mattbcowan.com>
2019-04-04 15:04:47 -0500

    Add Create Note and push to server

--- app/notes/notesRoute.js
+++ app/notes/notesRoute.js
@@ -4,16 +4,34 @@ const express = require('express'),
       Note = require('./notesModel.js')
 
 // Create a new Note
-router.post('/', notes.create);
+router.post('/', (req, res) => {
+  var title = req.body.title,
+      content = req.body.content,
+      newNote = {title: title, content: content}
+
+  Note.create(newNote, (err, newlyCreated) => {
+    if(err) {
+      console.log(err)
+    } else {
+      // redirect back to notes page
+      console.log(newlyCreated);
+      res.redirect('/notes');
+    }
+  })
+})
 
 // Retrieve all Notes
-// router.get('/', notes.findAll);
 router.get('/', (req, res) => {
   Note.find({}, (err, allNotes) => {
     if(err) {
       console.log(err);
     } else {
-      res.render('index', {notes: allNotes, title: 'Notes Page', message: 'Welcome to the notes page', noNotes: 'There are no notes right now.'})
+      res.render('index', {
+        notes: allNotes,
+        title: 'Notes Page',
+        message: 'Welcome to the notes page',
+        noNotes: 'There are no notes right now.'
+      })
     }
   });
 });
